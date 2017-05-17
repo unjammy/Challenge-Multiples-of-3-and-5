6433a8aa73e5e4f3faeab5897acebda4d0595776
clarkdenunzio <clark@clarkdenunzio.com>
2017-05-17 13:51:20 -1000

    first try

--- multiples.js
+++ multiples.js
@@ -8,5 +8,22 @@ exports.sumOfAMultiple = function( n ) {
 
   // do your work here
 
+  var leftside = 0;
+  var rightside = 0;
+
+  for(var i = 0; i <= n; i++){
+    if ( (i % 3) === 0 ){
+      leftside = leftside + i;
+    }
+  };
+
+  for(var j = 0; j <= n; j++){
+    if( (j % 5) === 0 ){
+      rightside = rightside + j;
+    }
+  };
+
+  sum = leftside + rightside;
+
   return sum;
 };
